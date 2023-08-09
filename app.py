a16b8c4611b09725742d88a8b2f7d15bfcd27d92
k10 <40850370+ketangangal@users.noreply.github.com>
2023-08-09 17:34:50 +0000

    created endpoints

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI
+from inference import preprocess, score, postprocess
 import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/get_schema")
+async def schema():
+    data = {'service': 0.9910802775024777,
+            'flag': 0.997636306334699,
+            'src_bytes': 491,
+            'dst_bytes': 0,
+            'count': 2,
+            'same_srv_rate': 1.0,
+            'diff_srv_rate': 0.0,
+            'dst_host_srv_count': 25,
+            'dst_host_same_srv_rate': 0.17,
+            'dst_host_same_src_port_rate': 0.17
+            }
+    return {"schema": data}
+
+
+@app.post("/predict")
+async def predict(data: dict):
+    list_values = preprocess(data)
+    result = score(list_values)
+    response = postprocess(result)
+    return {"Result": response[0]}
 
 
 if __name__ == "__main__":
--- inference.py
+++ inference.py
@@ -1,8 +1,23 @@
-def preprocess():
-    pass
+import os
+from joblib import load
 
-def score():
-    pass
 
-def postprocess():
-    pass
+def preprocess(data: dict) -> list:
+    """ This Function preprocess the data before scoring the model """
+    return list(data.values())
+
+
+def score(data: list) -> int:
+    """ This Function score the model """
+    path = os.path.join(os.getcwd(), "model_store", "model.pkl")
+    model = load(path)
+    response = model.predict([data])
+    return response
+
+
+def postprocess(result: list) -> str:
+    """ This Function converts the model ouput into human readable format """
+    path = os.path.join(os.getcwd(), "model_store", "label_encoder.pkl")
+    decoder = load(path)
+    response = decoder.inverse_transform(result)
+    return response
