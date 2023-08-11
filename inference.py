95abf0c880d7f80ca6ec8676eb9a067308c3fafd
k10 <40850370+ketangangal@users.noreply.github.com>
2023-08-11 16:01:52 +0000

    updates

--- inference.py
+++ inference.py
@@ -1,23 +1,28 @@
 import os
 from joblib import load
+import pandas as pd
 
 
 def preprocess(data: dict) -> list:
     """ This Function preprocess the data before scoring the model """
-    return list(data.values())
+    data_frame = pd.DataFrame(data)
+    print(data_frame)
+    path = os.path.join(os.getcwd(), "model_store", "scaler.pkl")
+    scaler = load(path)
+    return scaler.transform(data_frame)
 
 
 def score(data: list) -> int:
     """ This Function score the model """
     path = os.path.join(os.getcwd(), "model_store", "model.pkl")
     model = load(path)
-    response = model.predict([data])
+    response = model.predict(data)
     return response
 
 
 def postprocess(result: list) -> str:
     """ This Function converts the model ouput into human readable format """
-    path = os.path.join(os.getcwd(), "model_store", "label_encoder.pkl")
+    path = os.path.join(os.getcwd(), "model_store", "encoder.pkl")
     decoder = load(path)
     response = decoder.inverse_transform(result)
     return response
